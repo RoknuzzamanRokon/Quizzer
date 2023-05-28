b89b7002e5176c43d398de3b2b591578671608f9
Rokon Ron <rokon.raz@gmail.com>
2023-05-28 23:37:51 +0600

    JCAE

--- ui.py
+++ ui.py
@@ -9,14 +9,14 @@ class Structure:
 
         self.window = Tk()
         self.window.title(string="Quizzer")
-        self.window.config(padx=20, pady=20,)
-
-        self.canvas = Canvas()
-        self.canvas.config(width=300, height=250)
-        # self.canvas.canvas.create_text(250, 10, text="Score: ", width=250, font=("Arial", 15, "bold"))
-        self.canvas.create_text(150,125,text="This is a simple text", font=("Arial", 20, "italic"))
-        self.canvas.grid(row=0, column=0)
-
+        # self.window.config(padx=20, pady=20,)
+        #
+        # self.canvas = Canvas()
+        # self.canvas.config(width=300, height=250)
+        # # self.canvas.canvas.create_text(250, 10, text="Score: ", width=250, font=("Arial", 15, "bold"))
+        # self.canvas.create_text(150,125,text="This is a simple text", font=("Arial", 20, "italic"))
+        # self.canvas.grid(row=0, column=0)
+        #
 
         self.window.mainloop()
 

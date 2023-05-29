f8e35eccebd74492958c9033857348d19188c591
Rokon Ron <rokon.raz@gmail.com>
2023-05-29 14:14:17 +0600

    change for ui makeing and ask the user for next qustion.

--- ui.py
+++ ui.py
@@ -2,21 +2,42 @@ from tkinter import *
 
 THEME_COLOR = "#375362"
 
+IMAGE_01 = "images/true.png"
+IMAGE_02 = "images/false.png"
+
 
 class Structure:
 
-    def __int__(self):
+    def __init__(self):
 
         self.window = Tk()
-        self.window.title(string="Quizzer")
-        # self.window.config(padx=20, pady=20,)
-        #
-        # self.canvas = Canvas()
-        # self.canvas.config(width=300, height=250)
-        # # self.canvas.canvas.create_text(250, 10, text="Score: ", width=250, font=("Arial", 15, "bold"))
-        # self.canvas.create_text(150,125,text="This is a simple text", font=("Arial", 20, "italic"))
-        # self.canvas.grid(row=0, column=0)
-        #
+        self.window.title("Quizzer")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # Create a label.
+        self.label = Label()
+        self.label.config(text="Score:", fg="white", bg=THEME_COLOR, font=("Arial", 15, "bold"))
+        self.label.grid(row=0,column=1)
+
+        # Create Canvas.
+        self.canvas = Canvas()
+        self.canvas.config(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125,
+                                                     text="This is a simple text,just check what is happend.",
+                                                     font=("Arial", 20, "italic"),
+                                                     fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=30)
+
+        # Create True Button.
+        self.true_button = Button()
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button.config(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        # Create False Button.
+        self.false_button = Button()
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button.config(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
-
